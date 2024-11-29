4cb28303e1d27a965565db24a0f2d3a742bb8034
Natalia Somka <nataliia.somka@gmail.com>
2024-11-29 19:18:47 +0200

    add info of two days

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Notes />
     </div>
   );
+}
+
+function Notes() {
+  const backendData = [
+    { id: '1', text: 'Річниця Всеукраїнського референдуму про відділення від СРСР. Рівно 30 років тому  90% українців висловилися за Незалежність України.', url: 'https://w.wiki/8RcL' },
+    { id: '2', text: 'Ярослав Мудрий у 1015 році посів у Києві княжий престол', url: 'https://www.hroniky.com/news/view/13944-iaroslav-mudryi-u-1015-rotsi-posiv-u-kyievi-kniazhyi-prestol' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    { id: '6' },
+    { id: '7' },
+    { id: '8' },
+    { id: '9' },
+    { id: '10' },
+    { id: '11' },
+    { id: '12' },
+    { id: '13' },
+    { id: '14' },
+    { id: '15' },
+    { id: '16' },
+    { id: '17' },
+    { id: '18' },
+    { id: '19' },
+    { id: '20' },
+    { id: '21' },
+    { id: '22' },
+    { id: '23' },
+    { id: '24' },
+  ]
+
+  const noteRootStyle = {
+    border: "2px #0af solid",
+    borderRadius: 9,
+    margin: 20,
+    backgroundColor: "#efefef",
+    padding: 6
+  };
+
+  return (
+    <div style={{ width: 400 }}>
+      {backendData.map(day => 
+        <div style={noteRootStyle} key={day.id}>
+          <p>{day.text}</p>
+          <a href={day.url}>{day.url}</a>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
